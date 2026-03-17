e84e7da71bbaf777a0f4ec7e53554331903487c0
Shreyas Sivaraman <133583569+shreyas199231@users.noreply.github.com>
2026-03-17 13:32:02 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ThinkingPad from './ThinkingPad'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThinkingPad />
+  </React.StrictMode>
+)
