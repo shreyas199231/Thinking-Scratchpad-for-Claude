54909a4ea4a10a5227ea1d1e68f0e362d3ae5e13
Shreyas Sivaraman <133583569+shreyas199231@users.noreply.github.com>
2026-03-17 13:49:46 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ThinkingPad from './ThinkingPad'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThinkingPad />
-  </React.StrictMode>
-)
